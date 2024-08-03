95f7a6abe0c61542e969e4ab82a16d3c1705b649
RobsenGelalcha <robsengudeta1@outlook.com>
2024-08-03 14:23:53 +0300

    created math model and added lombok dependency

---
+++ src/main/java/com/github/models/History.java
@@ -0,0 +1,5 @@
+package com.github.models;
+
+public class History {
+    
+}
--- src/main/java/com/github/models/Math.java
+++ src/main/java/com/github/models/Math.java
@@ -1,8 +1,113 @@
 package com.github.models;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.util.Map;
+
+@Document(collection = "sat_questions")  
 public class Math {
+
+    @Id
+    private String id;  
+
+    private String answer;
     
+    private Map<String, String> choices;
+
+    private String exam;
+
+    private int questionId;
+
+    private int originalQuestionNumber;
+
+    private String question;
+
+    private int sectionLength;
+
+    private int sectionNumber;
+
+    private String[] tags;
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Map<String, String> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(Map<String, String> choices) {
+        this.choices = choices;
+    }
+
+    public String getExam() {
+        return exam;
+    }
+
+    public void setExam(String exam) {
+        this.exam = exam;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public int getOriginalQuestionNumber() {
+        return originalQuestionNumber;
+    }
+
+    public void setOriginalQuestionNumber(int originalQuestionNumber) {
+        this.originalQuestionNumber = originalQuestionNumber;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getSectionLength() {
+        return sectionLength;
+    }
+
+    public void setSectionLength(int sectionLength) {
+        this.sectionLength = sectionLength;
+    }
+
+    public int getSectionNumber() {
+        return sectionNumber;
+    }
+
+    public void setSectionNumber(int sectionNumber) {
+        this.sectionNumber = sectionNumber;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
 }
+
