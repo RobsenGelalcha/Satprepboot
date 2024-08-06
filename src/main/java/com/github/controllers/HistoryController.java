c623b7411125f13274e0f0f1b779f96125e6d5eb
RobsenGelalcha <robsengudeta1@outlook.com>
2024-08-06 20:52:02 +0300

    first steps for the history controller

--- src/main/java/com/github/controllers/HistoryController.java
+++ src/main/java/com/github/controllers/HistoryController.java
@@ -2,11 +2,33 @@ package com.github.controllers;
 
 import com.github.models.History;
 import com.github.services.HistoryService;
+import com.github.services.MathService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-
-
+import java.util.List;
+@RestController
+@RequestMapping("/sat_history")
 public class HistoryController {
-    
+    private final HistoryService historyService;
+
+
+    // @Autowired
+    // public MathController(MathService mathService) {
+    //     this.mathService = mathService;
+    // }
+
+    @Autowired
+    public HistoryController(HistoryService historyService){
+        this.historyService = historyService;
+    }
+    @GetMapping
+    public List<History> getAllHistoryQuestions(){
+        return historyService.findAll();
+    }
+
+    @GetMapping("/exam/{exam}")
+    public List<History> getHistoryByExam(@PathVariable String exam){
+        return HistoryService.findByExam(exam);
+    }
 }
